dc27d84226818fd1463b39ae09e5d099eade8521
Zeynep TANRIVERMİŞ <zeynep_tanrivermis@hotmail.com>
2024-03-18 13:57:58 +0300

    Mini-Projects

--- Employees/Employees.java
+++ Employees/Employees.java
@@ -3,7 +3,7 @@
 
 import java.util.Scanner;
 
-public class EmployeesProject {
+public class Employees {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
